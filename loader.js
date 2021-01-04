b2a25b4da3f4f825d7b59c289ed0fd549626e611
seblognya <58299035+seblognya@users.noreply.github.com>
2021-01-05 04:24:20 +0700

    Update loader.js

--- loader.js
+++ loader.js
@@ -1,4 +1,4 @@
-<script>
+
 var myVar;
 
 function myFunction() {
@@ -9,4 +9,3 @@ function showPage() {
   document.getElementById("loader").style.display = "none";
   document.getElementById("myDiv").style.display = "block";
 }
-</script>
